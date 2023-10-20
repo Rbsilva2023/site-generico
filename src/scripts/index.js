45adfa5677cafa56d937bffc0656bc4661b15136
Rbsilva2023 <robsonchristopher2022@gmail.com>
2023-10-20 15:28:30 -0300

    "Tarefa 4 concluída"

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,27 @@
+function handleModal () {
+    const button = document.querySelector("#showMessage")
+    const buttonTwo = document.querySelector("#showMessage__two")
+    const modalContainer = document.querySelector("#modal")
 
+    button.addEventListener('click', function(){
+        modalContainer.showModal()
+    })
+    buttonTwo.addEventListener('click', function(){
+        modalContainer.showModal()
+    })
+}
 
+function closeModal(){
+    const closeButton = document.querySelector("#button-x")
+    const modalContainer = document.querySelector("#modal")
 
+    closeButton.addEventListener('click', function(){
+        modalContainer.close()
+    })
+}
 
+handleModal()
+closeModal()
 
 
 
